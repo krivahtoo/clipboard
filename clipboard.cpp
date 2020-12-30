26c85f4b40a23e58bb3ee0297cad209b456a01b2
Noah Too <krivahtoo@gmail.com>
2020-12-30 13:52:26 +0300

    Added exception handling

--- clipboard.cpp
+++ clipboard.cpp
@@ -50,72 +50,76 @@ int main(int argc, char** argv)
         ("h,help", "Print usage")
         ("v,version", "Print version")
     ;
-    
+
     options.custom_help("[OPTIONS].. <file>");
+    try {
+      auto result = options.parse(argc, argv);
 
-    auto result = options.parse(argc, argv);
+      if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+      }
+      if (result.count("version")) {
+        std::cout << "Version " << VERSION << std::endl;
+        exit(0);
+      }
 
-    if (result.count("help")) {
-      std::cout << options.help() << std::endl;
-      exit(0);
-    }
-    if (result.count("version")) {
-      std::cout << "Version " << VERSION << std::endl;
-      exit(0);
-    }
+      std::string input, output, data;
+      if (result.count("input"))
+        input = result["input"].as<std::string>();
+      if (result.count("output"))
+        output = result["output"].as<std::string>();
 
-    std::string input, output, data;
-    if (result.count("input"))
-      input = result["input"].as<std::string>();
-    if (result.count("output"))
-      output = result["output"].as<std::string>();
+      if (input.empty() && output.empty()) {
+        std::cout << ERR_MSG << "You must specify either input and output file." << std::endl;
+        exit(1);
+      }
 
-    if (input.empty() && output.empty()) {
-      std::cout << ERR_MSG << "You must specify either input and output file." << std::endl;
-      exit(1);
-    }
-
-    if (input.empty()) {
-      if (clip::has(clip::text_format())) {
-        clip::get_text(data);
+      if (input.empty()) {
+        if (clip::has(clip::text_format())) {
+          clip::get_text(data);
+        } else {
+          std::cout << ERR_MSG << "Clipboard doesn't contain text" << std::endl;
+          exit(1);
+        }
       } else {
-        std::cout << ERR_MSG << "Clipboard doesn't contain text" << std::endl;
-        exit(1);
-      }
-    } else {
-      std::ifstream f(input);
-      if (!f.is_open()) {
-        // The file hasn't been opened; take appropriate actions here.
-        std::cout << ERR_MSG << "File could not be opened" << std::endl;
-        exit(1);
-      }
+        std::ifstream f(input);
+        if (!f.is_open()) {
+          // The file hasn't been opened; take appropriate actions here.
+          std::cout << ERR_MSG << "File could not be opened" << std::endl;
+          exit(1);
+        }
 
-      f.seekg(0, std::ios::end);
-      data.reserve(f.tellg());
-      f.seekg(0, std::ios::beg);
-      data.assign(std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>());
-      if (f.fail()) {
-        std::cout << ERR_MSG << "Could not copy file contents" << std::endl;
-        exit(1);
-      }
-    }
-
-    if (output.empty()) {
-      std::string out = data;
-      if (clip::set_text(out)) {
-        std::cout << "File copied to clipboard" << std::endl;
-      } else {
-        std::cout << ERR_MSG << "Could not copy to clipboard" << std::endl;
-      }
-    } else {
-      std::ofstream os(output);
-      if(os.is_open()){
-        os << data;
-        if (os.bad()) {
-          std::cout << ERR_MSG << "Could not write to file" << std::endl;
+        f.seekg(0, std::ios::end);
+        data.reserve(f.tellg());
+        f.seekg(0, std::ios::beg);
+        data.assign(std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>());
+        if (f.fail()) {
+          std::cout << ERR_MSG << "Could not copy file contents" << std::endl;
           exit(1);
         }
       }
+
+      if (output.empty()) {
+        std::string out = data;
+        if (clip::set_text(out)) {
+          std::cout << "File copied to clipboard" << std::endl;
+        } else {
+          std::cout << ERR_MSG << "Could not copy to clipboard" << std::endl;
+        }
+      } else {
+        std::ofstream os(output);
+        if(os.is_open()){
+          os << data;
+          if (os.bad()) {
+            std::cout << ERR_MSG << "Could not write to file" << std::endl;
+            exit(1);
+          }
+        }
+      }
+    } catch (std::exception& e) {
+      std::cout << ERR_MSG << e.what() << std::endl;
+      exit(1);
     }
 
     return 0;
